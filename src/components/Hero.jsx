951d644c7fa0e82f1d54a209ddfb1e9412d18f14
James Brooks <james@alt-three.com>
2023-01-16 16:08:30 +0000

    Add photos to homepage

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,42 @@
 import Image from 'next/image'
+import clsx from 'clsx'
 
 import { Button } from '@/components/Button'
 import { Container } from '@/components/Container'
 import backgroundImage from '@/images/background.jpg'
+
+import image1 from '@/images/events/phpstoke1.jpg'
+import image2 from '@/images/events/phpstoke2.jpg'
+import image3 from '@/images/events/phpstoke3.jpg'
+import image4 from '@/images/events/phpstoke4.jpg'
+import image5 from '@/images/events/phpstoke5.jpg'
+
+function Photos() {
+  let rotations = ['rotate-2', '-rotate-2', 'rotate-2', 'rotate-2', '-rotate-2']
+
+  return (
+    <div className="mt-16 sm:mt-20">
+      <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
+        {[image1, image2, image3, image4, image5].map((image, imageIndex) => (
+          <div
+            key={image.src}
+            className={clsx(
+              'relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl',
+              rotations[imageIndex % rotations.length]
+            )}
+          >
+            <Image
+              src={image}
+              alt=""
+              sizes="(min-width: 640px) 18rem, 11rem"
+              className="absolute inset-0 h-full w-full object-cover"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
 
 export function Hero() {
   return (
@@ -59,6 +93,7 @@ export function Hero() {
           </dl>
         </div>
       </Container>
+      <Photos />
     </div>
   )
 }
