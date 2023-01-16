36f7050009e2e584358ed03a78d442bffa0453f5
James Brooks <james@alt-three.com>
2023-01-16 15:17:44 +0000

    wip

--- src/components/Hosts.jsx
+++ src/components/Hosts.jsx
@@ -1,6 +1,8 @@
 import { Container } from '@/components/Container'
 import Image from 'next/image'
 
+import { GlobeAltIcon } from '@/components/GlobeAltIcon'
+
 import andyImage from '@/images/avatars/andy-jones.jpg'
 import jamesImage from '@/images/avatars/james-brooks.jpg'
 import richImage from '@/images/avatars/rich-jones.jpg'
@@ -10,11 +12,11 @@ const people = [
     name: 'James Brooks',
     image:jamesImage,
     twitterUrl: 'https://twitter.com/jbrooksuk',
+    website: 'https://james.brooks.page/?ref=phpstoke',
     role: 'Developer at Laravel'
   }, {
     name: 'Andy Jones',
     image: andyImage,
-    twitterUrl: 'https://twitter.com/andyjoneski',
     website: 'https://awaredigital.co.uk/?ref=phpstoke',
     role: 'Director at Aware Digital',
   }, {
@@ -67,6 +69,15 @@ export function Hosts() {
                               <svg className="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
                                 <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
                               </svg>
+                            </a>
+                          </li>
+                        }
+                        {
+                          person.website &&
+                          <li>
+                            <a href={person.website} className="text-gray-400 hover:text-gray-300">
+                              <span className="sr-only">Website</span>
+                              <GlobeAltIcon className="h-5 w-5" />
                             </a>
                           </li>
                         }
