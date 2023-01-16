ffc80cee0e8ff70d48afb406d94675db13067a03
James Brooks <james@alt-three.com>
2023-01-16 17:22:54 +0000

    wip

--- src/components/Speakers.jsx
+++ src/components/Speakers.jsx
@@ -175,29 +175,25 @@ export function Speakers() {
                         />
                       </div>
                     </div>
-                    <div className="space-y-2 xl:flex xl:items-center xl:justify-between mt-8">
-                      <div>
-                        <h3 className="font-display text-xl font-bold tracking-tight text-slate-900">
-                          {speaker.name}
-                        </h3>
+                    <div className="space-y-2 space-x-4 xl:flex xl:items-center xl:justify-between mt-8">
+                      <div className="space-y-1 text-lg font-medium leading-6">
+                        <h3 className="text-indigo-900">{speaker.name}</h3>
+                        <p className="mt-1 text-base tracking-tight text-slate-500">{speaker.role}</p>
                       </div>
                       <ul role="list" className="flex justify-center space-x-5">
-                      {
-                        speaker.twitterUrl &&
-                        <li>
-                          <a href={speaker.twitterUrl} className="text-gray-400 hover:text-gray-300">
-                            <span className="sr-only">Twitter</span>
-                            <svg className="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                              <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
-                            </svg>
-                          </a>
-                        </li>
-                      }
-                    </ul>
+                        {
+                          speaker.twitterUrl &&
+                          <li>
+                            <a href={speaker.twitterUrl} className="text-gray-400 hover:text-gray-300">
+                              <span className="sr-only">Twitter</span>
+                              <svg className="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                                <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
+                              </svg>
+                            </a>
+                          </li>
+                        }
+                      </ul>
                     </div>
-                    <p className="mt-1 text-base tracking-tight text-slate-500">
-                      {speaker.role}
-                    </p>
                   </div>
                 ))}
               </Tab.Panel>
