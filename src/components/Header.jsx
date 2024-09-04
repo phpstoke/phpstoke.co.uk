58210f0572e99e8e80a7297d8f03302cdc83015b
James Brooks <james@alt-three.com>
2024-09-04 10:49:44 +0100

    Use Link component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,15 @@ import { DiamondIcon } from '@/components/DiamondIcon'
 import { Icon } from '@/components/Icon'
 
 import event from "@/event";
+import Link from "next/link";
 
 export function Header() {
   return (
     <header className="relative z-50 pb-11 lg:pt-11">
       <Container className="flex flex-wrap items-center justify-center sm:justify-between lg:flex-nowrap">
-        <a className="mt-10 lg:mt-0 lg:grow lg:basis-0" href="/">
+        <Link className="mt-10 lg:mt-0 lg:grow lg:basis-0" href="/">
           <Icon className="h-24 sm:h-12 w-auto" />
-        </a>
+        </Link>
         <div className="order-first -mx-4 flex flex-auto basis-full overflow-x-auto whitespace-nowrap border-b border-indigo-600/10 py-4 font-mono text-sm text-indigo-600 sm:-mx-6 lg:order-none lg:mx-0 lg:basis-auto lg:border-0 lg:py-0">
           <div className="mx-auto flex items-center gap-2 px-4 overflow-hidden sm:gap-4">
             <p>
