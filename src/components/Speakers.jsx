cdd46fa23f903972e52cf3ea6136910d31b37ad8
James Brooks <james@alt-three.com>
2024-09-11 09:35:08 +0100

    Update Twitter links to X

--- src/components/Speakers.jsx
+++ src/components/Speakers.jsx
@@ -411,10 +411,10 @@ function SpeakerProfile({id, speaker, speakerIndex, ...props}) {
             {
               speaker.twitterUrl &&
               <li>
-                <a href={`https://twitter.com/${speaker.twitterUrl}`} className="text-gray-400 hover:text-gray-300">
-                  <span className="sr-only">Twitter</span>
-                  <svg className="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
+                <a href={`https://x.com/${speaker.twitterUrl}`} className="text-gray-400 hover:text-gray-300">
+                  <span className="sr-only">𝕏 (Ex. Twitter)</span>
+                  <svg className="size-5" aria-hidden="true" fill="currentColor" viewBox="0 0 300 300">
+                    <path d="M178.57 127.15 290.27 0h-26.46l-97.03 110.38L89.34 0H0l117.13 166.93L0 300.25h26.46l102.4-116.59 81.8 116.59h89.34M36.01 19.54H76.66l187.13 262.13h-40.66"/>
                   </svg>
                 </a>
               </li>
